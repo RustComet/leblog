e5fc041b992fb0533b7a5ad18bbe657defcdc4c4
Isaac Norman <isaacdnorman@gmail.com>
2013-05-21 22:02:42 +1000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leblog::Application.config.secret_token = 'b2f4cd180ac4cac522ea6db7a2ce323e245d5bbe67a8692152cec7728fa919490dd17964ddfbad1a41f6012eaa361f148ec175e8dfc35cf0f21a39b6d122de3a'
